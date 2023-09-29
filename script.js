8e9950a9931971f220980dd9576f4c15753475e4
Ahmadihsan3 <46346558+Ahmadihsan3@users.noreply.github.com>
2023-09-29 10:17:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var slideIndex = slides;
+showSlide(slideIndex);
+
+
+function nextslide(n){
+showSlide(slideIndex += n);
+}
+
+function showSlide(n){
+var i;
+var slides =  document.getElementsByClassName("images");
+
+
+if (n > slides.length) {
+    slideIndex = 1
+}
+
+if (n < 1){
+  slideIndex = slides.length;
+}
+
+for (i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+}
+
+slides[slideIndex - 1].style.display = "block";
+}
